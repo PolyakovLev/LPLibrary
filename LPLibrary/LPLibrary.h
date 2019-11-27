00d0a5eb48ddb563d5b3231868d4cd0398dd854a
Lev Polyakov <www.goldfox@gmail.com>
2019-11-27 22:42:50 +0300

    get started

---
+++ LPLibrary/LPLibrary.h
@@ -0,0 +1,19 @@
+//
+//  LPLibrary.h
+//  LPLibrary
+//
+//  Created by Lev polyakov on 27.11.2019.
+//  Copyright © 2019 Lev polyakov. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for LPLibrary.
+FOUNDATION_EXPORT double LPLibraryVersionNumber;
+
+//! Project version string for LPLibrary.
+FOUNDATION_EXPORT const unsigned char LPLibraryVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LPLibrary/PublicHeader.h>
+
+
---
+++ LPLibraryExamples/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LPLibraryExamples
+//
+//  Created by Lev polyakov on 27.11.2019.
+//  Copyright © 2019 Lev polyakov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
