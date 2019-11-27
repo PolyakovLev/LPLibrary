f045ade8384c80f08f8eab888c66cc24905702d5
Lev Polyakov <www.goldfox@gmail.com>
2019-11-27 23:16:03 +0300

    add some functions and tests

---
+++ LPLibrary/LPLibrary.swift
@@ -0,0 +1,21 @@
+//
+//  LPLibrary.swift
+//  LPLibrary
+//
+//  Created by Lev polyakov on 27.11.2019.
+//  Copyright © 2019 Lev polyakov. All rights reserved.
+//
+
+import Foundation
+
+public final class LPLibrary {
+    let name = "LPLibrary"
+    
+    public func sum(a: Int, b: Int) -> Int {
+        return a + b
+    }
+    
+    public func sub(a: Int, b: Int) -> Int {
+        return a - b
+    }
+}
